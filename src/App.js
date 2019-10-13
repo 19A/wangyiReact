3e9e65fa8ee259389e13009e581a1aa24d3e3777
zhubiao <1967349734@qq.com>
2019-10-13 14:57:16 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter,Switch,Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/home";
+import Category from "./pages/category";
+import Reco from "./pages/reco";
+import Cart from "./pages/cart";
+import Profile from "./pages/profile";
+
+import Footer from "./components/footer";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route  path="/category" component={Category}></Route>
+            <Route  path="/reco" component={Reco}></Route>
+            <Route  path="/cart" component={Cart}></Route>
+            <Route  path="/profile" component={Profile}></Route>
+            <Route  path="/" component={Home}></Route>
+          </Switch>
+        </BrowserRouter>
+        <Footer/>
+      </div>
+     
+      
+    )
+  }
 }
-
-export default App;
